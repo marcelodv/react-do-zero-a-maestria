2327a44cf1ebc9cd6585f7df73e3a5dbbf37a53f
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-02-02 21:36:43 -0300

    feat: create my first component

---
+++ react-fundamentals/fundamentals/src/App.jsx
@@ -0,0 +1,17 @@
+
+// Components / JSX
+import FirstComponent from './components/FirstComponent'
+
+// Styles / CSS
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <FirstComponent />
+    </div>
+  )
+}
+
+export default App
