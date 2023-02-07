fe6f8a4a8822cd67e88be5c61ded523056f5c29b
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-02-06 23:40:36 -0300

    feat: create a project to learn about images in react

---
+++ advancing-in-react/public-images/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Straw from './assets/STRAWhat.png'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Luffy :p</h1>
+      <div className="img">
+        <img src="Luffy.png" alt="Just Luffy from One Piece" />
+        <img src={Straw} alt="Straw Hat from One Piece" />
+      </div>
+    </div>
+  )
+}
+
+export default App
