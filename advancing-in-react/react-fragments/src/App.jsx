ec0fd650cbd3b750f36d1bea3cdc33126fed7c7a
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-02-10 00:25:43 -0300

    feat: created a project to learn about react fragments

---
+++ advancing-in-react/react-fragments/src/App.jsx
@@ -0,0 +1,14 @@
+import ReactFragments from './components/ReactFragments'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>React Fragments</h1>
+      <ReactFragments />
+    </div>
+  )
+}
+
+export default App
