4132369cee04d5d56efb030113c0a5c4a55807d3
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-02-21 09:07:35 -0300

    feat: label tag wrapping input tag

--- react-forms/form/src/components/MyForm.jsx
+++ react-forms/form/src/components/MyForm.jsx
@@ -8,6 +8,10 @@ export default function MyForm () {
                     <label htmlFor="name">Name: </label>
                     <input type="text" name="name" id="name" />
                 </div>
+                <label>
+                    <span>E-mail: </span>
+                    <input type="email" name="name" id="name" />
+                </label>
                 <input type="submit" value="Submit" />
             </form>
         </div>
