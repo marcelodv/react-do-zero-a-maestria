bb668ac1cd65549688f64235d856163a375e7710
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-02-21 19:11:20 -0300

    feat: better approach to set a input value using useState

--- react-forms/form/src/components/MyForm.jsx
+++ react-forms/form/src/components/MyForm.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react"
 import "./MyForm.css"
 
 export default function MyForm () {
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
     return (
         <div>
             <form>
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" id="name" />
+                    <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
                 </div>
                 <label>
                     <span>E-mail: </span>
-                    <input type="email" name="name" id="name" />
+                    <input type="email" name="email" id="email" onChange={(e) => setEmail(e.target.value)} />
                 </label>
                 <input type="submit" value="Submit" />
             </form>
