3264bd81c477f64bcc6887c020199d502514d6c2
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-03-06 09:11:52 -0300

    feat: add dynamic loading in product list

--- req-http/src/App.jsx
+++ req-http/src/App.jsx
@@ -7,23 +7,78 @@ const urlBase = "http://localhost:3333/products";
 export default function App() {
   const [products, setProducts] = useState([]);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
   useEffect(() => {
     async function fetchData () {
       const res = await fetch(urlBase);
-      let data = await res.json();
+      const data = await res.json();
+
       setProducts(data);
     }
     fetchData();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price
+    };
+
+    const res = await fetch(urlBase, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(product),
+    });
+
+    const addedProduct = await res.json();
+
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("")
+    setPrice("")
+  }
+
   return (
     <div className="App">
-      <h1>Product's List</h1>
+      <h2>Product's List</h2>
       <ul>
         {products.map((item) => (
           <li key={item.id}>{item.name} - ${item.price}</li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:&nbsp;
+            <input 
+              type="text" 
+              name="name" 
+              id="name" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <br />
+          <label>
+            Price:&nbsp;
+            <input 
+              type="number" 
+              name="price" 
+              id="price" 
+              value={price} 
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <br />
+          <input type="submit" value="Add Product" />
+        </form>
+      </div>
     </div>
   )
 }
