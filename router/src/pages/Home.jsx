d470b376879a6e74fb5876c83b4514639b3161a0
Marcelo Santos <marcelo.santos.dev@gmail.com>
2023-03-08 00:01:37 -0300

    feat: implement json-server and useFetch custom hook

--- router/src/pages/Home.jsx
+++ router/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
 export default function Home () {
+    const url = "http://localhost:3333/products";
+
+    const {data: items, loading, error} = useFetch(url)
+
     return (
-        <h2>Home</h2>
+        <div>
+            <h2>Products</h2>
+            {error && <p>{error}</p>}
+            <ul>
+                {items && items.map(item => (
+                    <li key={item.id}>{item.name} - ${item.price}</li>
+                ))}
+            </ul>
+        </div>
     )
 }
